389f3d3c67b8ee71752d295b8d1b5307dbbc4bd6
ATovmasyan <arshtov94@gmail.com>
2022-01-26 18:58:17 +0400

    change point checker logic

--- zone_manager/src/main/java/com/codulate/admin/service/ZoneIntersectionChecker.java
+++ zone_manager/src/main/java/com/codulate/admin/service/ZoneIntersectionChecker.java
@@ -1,52 +1,22 @@
 package com.codulate.admin.service;
 
 import com.codulate.dto.PointDTO;
+
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class ZoneIntersectionChecker {
 
     public static boolean contains(PointDTO pointDTO, List<PointDTO> points) {
-        double minimumXAxis = findMinimumAxis(getXAxisFromPointsDto(points));
-        double minimumYAxis = findMinimumAxis(getYAxisFromPointsDto(points));
-        double minimumZAxis = findMinimumAxis(getZAxisFromPointsDto(points));
-        double maximumXAxis = findMaximumAxis(getZAxisFromPointsDto(points));
-        double maximumYAxis = findMaximumAxis(getZAxisFromPointsDto(points));
-        double maximumZAxis = findMaximumAxis(getZAxisFromPointsDto(points));
-
-        return pointDTO.getxAxis() <= maximumXAxis && pointDTO.getxAxis() >= minimumXAxis &&
-                pointDTO.getyAxis() <= maximumYAxis && pointDTO.getyAxis() >= minimumYAxis &&
-                pointDTO.getzAxis() <= maximumZAxis && pointDTO.getzAxis() >= minimumZAxis;
-    }
-
-    private static double findMinimumAxis(List<Double> axis){
-        double minimumAxis = axis.get(0);
-        for (Double ax: axis) {
-            if (ax < minimumAxis)
-                minimumAxis = ax;
+        int i;
+        int j;
+        boolean result = false;
+        for (i = 0, j = points.size() - 1; i < points.size(); j = i++) {
+            if ((points.get(i).getyAxis() > pointDTO.getyAxis()) != (points.get(j).getyAxis() > pointDTO.getyAxis()) &&
+                    (pointDTO.getxAxis() < (points.get(j).getxAxis() - points.get(i).getxAxis()) *
+                            (pointDTO.getyAxis() - points.get(i).getyAxis()) / (points.get(j).getyAxis() - points.get(i).getyAxis()) + points.get(i).getxAxis())) {
+                result = !result;
+            }
         }
-        return minimumAxis;
+        return result;
     }
-
-    private static double findMaximumAxis(List<Double> axis){
-        double maxAxis = axis.get(0);
-        for (Double ax: axis) {
-            if (ax > maxAxis)
-                maxAxis = ax;
-        }
-        return maxAxis;
-    }
-
-    private static List<Double> getXAxisFromPointsDto (List<PointDTO> points){
-       return points.stream().map(PointDTO::getxAxis).collect(Collectors.toList());
-    }
-
-    private static List<Double> getYAxisFromPointsDto (List<PointDTO> points){
-        return points.stream().map(PointDTO::getyAxis).collect(Collectors.toList());
-    }
-
-    private static List<Double> getZAxisFromPointsDto (List<PointDTO> points){
-        return points.stream().map(PointDTO::getzAxis).collect(Collectors.toList());
-    }
-
 }
